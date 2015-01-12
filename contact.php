8593e006c3784fafe45c6191d134e621388b35eb
Ketan Bhatt <ktbt10@gmail.com>
2015-01-13 02:18:25 +0530

    Contact.php
    Finalized contact.php

--- contact.php
+++ contact.php
@@ -3,33 +3,14 @@
 		require 'inc/header.php';
 ?>
 
-<div class="container">
-	<h2>Contact Us</h2>
-	<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-		Duis sed urna a tellus mollis hendrerit. Aliquam rhoncus 
-		aliquet sapien, non pellentesque felis.
-		Cras justo libero, v
-		iverra id odio in, vehicula ultricies massa. Curabitur diam tellus, 
-		posuere et eleifend vehicula.
-		Cras justo libero, v
-		iverra id odio in, vehicula ultricies massa. Curabitur diam tellus, 
-		posuere et eleifend vehicula. 
-	</p>
-	<p>
-		Cras justo libero, v
-		iverra id odio in, vehicula ultricies massa. Curabitur diam tellus, 
-		posuere et eleifend vehicula.
-		Cras justo libero, v
-		iverra id odio in, vehicula ultricies massa. Curabitur diam tellus, 
-		posuere et eleifend vehicula.
-	</p>
-</div>
+<div class="contact-page">
 
 <?php 
 	
 		require 'inc/form.php';
 ?>
 
+</div>
 
 
 
